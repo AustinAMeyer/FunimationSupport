987cbd307f0f27f90866b93b22b6228a08c2449c
Austin Meyer <Austin.a.Meyer@gmail.com>
2019-07-20 21:38:09 -0500

    Initial commit

--- FunimationSupportContact/ViewController.h
+++ FunimationSupportContact/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *funimationLabel;
+- (IBAction)callSupport:(id)sender;
+- (IBAction)emailSupport:(id)sender;
 
 @end
 
--- FunimationSupportContact/ViewController.m
+++ FunimationSupportContact/ViewController.m
@@ -13,11 +13,31 @@
 @end
 
 @implementation ViewController
-
+// #D8D8D8
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    CALayer *bottomBorder = [CALayer layer];
+    bottomBorder.borderColor = [UIColor colorWithRed:0.376 green:0.0 blue:0.827 alpha:1.0].CGColor;
+    bottomBorder.borderWidth = 100;
+    bottomBorder.frame = CGRectMake(1, CGRectGetHeight(_funimationLabel.frame)-1, CGRectGetWidth(_funimationLabel.frame), 3);
+    _funimationLabel.clipsToBounds = YES;
+    [_funimationLabel.layer addSublayer:bottomBorder];
 }
 
+
+- (IBAction)callSupport:(id)sender {
+    UIApplication *application = [UIApplication sharedApplication];
+    [application openURL:[NSURL URLWithString: @"tel:11111111111"] options:@{} completionHandler:nil];
+}
+
+- (IBAction)emailSupport:(id)sender {
+    NSString *recipients = @"mailto:help@funimation.com?subject=I need help";
+    NSString *body = @"&body=HI, \n\n I need help with: \n\n Thank you!";
+    
+    NSString *email = [NSString stringWithFormat:@"%@%@", recipients, body];
+    email = [email stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLFragmentAllowedCharacterSet]];
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:email] options:@{} completionHandler:nil];
+}
 
 @end
